5a7d51ea3ab6d110cfdcb97a012cf56a8f035973
Alepse <kenespela@gmail.com>
2025-05-14 11:42:40 +0800

    changes to the link and  thumbnail of blog website project

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ const projectsData = [
   },
   {
     title: "Internship Blog",
-    link: "https://blog-internship.vercel.app/",
+    link: "https://blog-wfg8.vercel.app/",
     description: "A blog website for my documentation of my internship experience in a government agency, Department of Science and Technology Region 5 (DOST-V).",
     tech: ["React.js", "Tailwind CSS","Javascript"],
     type: "Frontend",
